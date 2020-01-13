8f1acb36b9040176748edbab7236c7aaed8711bb
VickCho <46130849+VickCho@users.noreply.github.com>
2020-01-13 22:14:34 +0200

    Add files via upload

---
+++ fivembg-load/__resource.lua
@@ -0,0 +1,14 @@
+-- FiveM Bulgaria Created By VickCho, Arabadjiew --
+
+
+files {
+    'index.html',
+    'style.css',
+    'music/music.ogg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+-- FiveM Bulgaria Created By VickCho, Arabadjiew --
